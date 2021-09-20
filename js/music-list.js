5a0bf1400f5d10fd9a7a18cdbd45379581403fce
Muneer Alam <77039639+Muneer320@users.noreply.github.com>
2021-09-20 11:47:20 +0530

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -8,52 +8,58 @@
 //   }
 //paste it inside the array as more as you want music then you don't need to do any other thing
 let allMusic = [
-    {
-      name: "Let It Go (Voilin Cover)",
-      artist: "Taylor Davis",
-      img: "music-1",
-      src: "music-1"
-    },
-    {
-      name: "Faded",
-      artist: "Alan Walker",
-      img: "music-2",
-      src: "music-2"
-    },
-    {
-      name: "Bad Romance",
-      artist: "Lady Gaga",
-      img: "music-3",
-      src: "music-3"
-    },
-    {
-      name: "CLoser",
-      artist: "Mike Archangelo",
-      img: "music-4",
-      src: "music-4"
-    },
-    {
-      name: "Despacito",
-      artist: "Luic Fonci, Daddy Yankee",
-      img: "music-5",
-      src: "music-5"
-    },
-    {
-      name: "Savage Love",
-      artist: "Jason Darulo",
-      img: "music-6",
-      src: "music-6"
-    },
-    {
-      name: "Believer",
-      artist: "Imagine Drangons",
-      img: "music-7",
-      src: "music-7"
-    },
-    {
-      name: "Perfect Two",
-      artist: "Auburn",
-      img: "music-8",
-      src: "music-8"
-    }
-  ];
+  {
+    name: "Thunder",
+    artist: "Imagine Dragons",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Faded",
+    artist: "Alan Walker",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Everything at Once",
+    artist: "Lenka",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "CLoser",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Despacito",
+    artist: "Luic Fonci, Daddy Yankee",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Savage Love",
+    artist: "Jason Darulo",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Believer",
+    artist: "Imagine Drangons",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Perfect Two",
+    artist: "Auburn",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Cheap Thrills",
+    artist: "Sia",
+    img: "music-9",
+    src: "music-9"
+  },
+];
